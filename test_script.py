caa2888ad7bc0c270a4f262aa06fdfcdf149eef4
Jiano Freo <jianofreo.magtangob@tup.edu.ph>
2026-06-20 21:59:29 +0800

    high level api

--- test_script.py
+++ test_script.py
@@ -1,31 +1,9 @@
-from neural_network_draw import NeuralNetwork, DrawNN
-# --- Example 2: full new features, low-level API ---
-network = NeuralNetwork(4, title="Handwritten Digit Classifier")
+# --- Example 3: high-level DrawNN, one call ---
+from neural_network_draw import DrawNN
 
-network.add_layer(3, labels=["pixel_1", "pixel_2", "pixel_3"])
-
-network.add_layer(
-    4,
-    weights=[
-        [0.10, -0.20, 0.30, 0.40],
-        [0.50, 0.60, -0.70, 0.80],
-        [-0.10, 0.20, 0.30, -0.40],
-    ],
-    biases=[0.10, 0.20, 0.30, 0.40],
-    activation="ReLU",
-)
-
-network.add_layer(
-    2,
-    weights=[
-        [0.20, -0.30],
-        [0.10, 0.40],
-        [0.50, -0.60],
-        [0.20, 0.10],
-    ],
-    biases=[0.05, -0.05],
-    activation="Softmax",
-    labels=["Cat", "Dog"],
-)
-
-network.draw(show_weights=True)
+DrawNN(
+    [4, 6, 6, 3],
+    input_labels=["sepal_len", "sepal_wid", "petal_len", "petal_wid"],
+    output_labels=["Setosa", "Versicolor", "Virginica"],
+    title="Iris Classifier Architecture",
+).draw()
