3ebd30c540eed930820a8714f9e0cba510bf3689
Jiano Freo <jianofreo.magtangob@tup.edu.ph>
2026-06-20 22:53:02 +0800

    **Version Bump & Documentation Update**
    Updated version to 0.2.0 and added images for documentation of `DrawNN` and labeled structure.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="neural-network-draw",
-    version="0.1.0",
+    version="0.2.0",
     description="Draw basic feed-forward neural network architecture diagrams with matplotlib",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
